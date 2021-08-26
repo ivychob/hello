0c301912faf10a38dd6497bef50fb89ead3e80e5
Ivy Chobanyan <ichobanyan2@huskers.unl.edu>
2021-08-26 19:18:23 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Ivy Chobanyan
+ *         ichobanyan2@huskers.unl.edu
+ * Date: 2020/08/24
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Ivy Chobanyan!\n");
+
+  return 0;
+}
